42816fbadc47bd566353d8c2402506fce42eb660
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-06-14 17:28:33 -0400

    adding first mutation endpoint

---
+++ src/component/input.tsx
@@ -0,0 +1,4 @@
+
+export function Input(props: React.ComponentPropsWithoutRef<"input">){
+    return <input {...props} type="text" className="border border-grey-800 dark:text-gray-800 px-4 py-2"></input>
+}
