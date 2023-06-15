37960ed76ee6f8a00372d163b037fae7de842e86
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-06-15 15:46:16 -0400

    Inital setup gen-image but having issue w/ fetch

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,17 +1,61 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import {
-  createTRPCRouter, publicProcedure} from "~/server/api/trpc";
+import {createTRPCRouter, protectedProcedure} from "~/server/api/trpc";
+import{ Configuration, OpenAIApi } from "openai";
+import { env } from "~/env.mjs";
+  
+const configuration = new Configuration({
+      apiKey: env.DALLE_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  
 
 export const generateRouter = createTRPCRouter({
-    generateIcon: publicProcedure.input(
+    generateIcon: protectedProcedure
+    .input(
         z.object({
-            prompt:z.string(),
+            prompt: z.string(),
         })
-    ).mutation(({ctx, input}) =>{
-        console.log("we are here", input.prompt)
-        return {
-            message: "success",
+    ).mutation( async ({ctx, input}) =>{
+
+        // TODO: Verify the user has enough credits 
+        const {count} = await ctx.prisma.user.updateMany({
+            where: {
+                id: ctx.session.user.id,  // TODO: replace with a real id
+                credits: {
+                    gte: 1,
+                },  
+            },
+            data:{
+                credits: {
+                    decrement: 1,
+                }
+            }
+        });
+
+        if (count <= 0){
+            throw new TRPCError({
+                code: 'BAD_REQUEST',
+                message: 'You do not have enough credits'
+            })
         }
+
+        //TODO: make a fetch request to DALLE API
+        const response = await openai.createImage({
+            prompt: input.prompt,
+            n: 1,
+            size: "1024x1024",
+          });
+
+          console.log("RESPONSE", response)
+
+          const url = response.data.data[0]?.url
+
+          console.log("URL", url)
+
+        return {
+            imageUrl: url,
+        };
     }), 
 });
