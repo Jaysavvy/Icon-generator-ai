077f9dc5152702147e33c9e3cae4599b3fab7b0f
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-06-18 15:05:05 -0400

    fixed next image b64 issue

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import {createTRPCRouter, protectedProcedure} from "~/server/api/trpc";
 import{ Configuration, OpenAIApi } from "openai";
 import { env } from "~/env.mjs";
+import { b64Image } from "~/data/b64Image";
   
 const configuration = new Configuration({
       apiKey: env.DALLE_API_KEY,
@@ -11,8 +12,8 @@ const configuration = new Configuration({
   const openai = new OpenAIApi(configuration);
   
   async function generateIcon(prompt: string): Promise<string | undefined> {
-    if (env.MOCK_DALLE = "true" ){
-        return "https://oaidalleapiprodscus.blob.core.windows.net/private/org-9BOvHkiD8ni6EG1FqaTun1pZ/user-DAoluL6N78SV4zTCSmjJ7gOg/img-UnFNVUSAIDSTaijhBXvK3NIG.png?st=2023-06-16T21%3A39%3A39Z&se=2023-06-16T23%3A39%3A39Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-06-16T20%3A51%3A37Z&ske=2023-06-17T20%3A51%3A37Z&sks=b&skv=2021-08-06&sig=lMXdZ7ylVTnmHeScrnJTTONl4i/Q6dM16XSvu03ZpqY%3D"
+    if (env.MOCK_DALLE == "true" ){
+        return b64Image
     } else {
         const response = await openai.createImage({
             prompt,
@@ -20,9 +21,7 @@ const configuration = new Configuration({
             size: "512x512",
             response_format: "b64_json",
           });
-          console.log("---")
-          console.log(response.data.data[0]?.b64_json)
-          console.log("---")
+          
           return response.data.data[0]?.b64_json;
     }
   }
